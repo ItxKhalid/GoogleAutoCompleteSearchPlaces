e5a85f8797fe053da62a9a1ef2dbebe3e5829442
ItxKhalid <mkhalidkhalid25@gmail.com>
2023-10-24 10:38:41 +0000

    Code Synced by DhiWise

---
+++ lib/presentation/address_screen/controller/address_controller.dart
@@ -0,0 +1,10 @@
+import 'package:e_com/core/app_export.dart';
+import 'package:e_com/presentation/address_screen/models/address_model.dart';
+
+/// A controller class for the AddressScreen.
+///
+/// This class manages the state of the AddressScreen, including the
+/// current addressModelObj
+class AddressController extends GetxController {
+  Rx<AddressModel> addressModelObj = AddressModel().obs;
+}
